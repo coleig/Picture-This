2e0d0136ac5cf42ac0567c7975c99694ce9636fd
coleig <46797131+coleig@users.noreply.github.com>
2019-03-22 19:03:22 -0700

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,3 +1,9 @@
+// I really like your project! Your files are all nicely organized but there is some missing documentation. When it's there, 
+// it's clear and really helps show what you're attempting to do at a glance. There are a lot of unused sections of code and 
+// imports, and sections of code doing very similar/the same things, which I would tidy up on the next pass. Your code 
+// flows nicely otherwise and I think future iterations could clean up the concurrency issues and some of your logic
+// so your components are structured more efficiently. 
+
 import { Meteor } from "meteor/meteor";
 
 import "../imports/api/messages.js";
